9b86ff8ae94a66d339fb9b3586f66306d49f25df
Amethist <77743895+meth1337@users.noreply.github.com>
2022-06-29 22:17:16 +0300

    1.1.1 // Installation scripts // Edit

--- build-binaries.sh
+++ build-binaries.sh
@@ -1,8 +1,10 @@
-pyinstaller -F --onefile clock.py
+pyinstaller -F --clean clock.py
 cd dist
 cp clock ..
 cd ..
 rm -rf clock.spec dist build
 mv clock rice-clock
+clear
 echo "Done!"
 echo "Now run create-command.sh."
+
