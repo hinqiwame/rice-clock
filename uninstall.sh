e24f981be270d899899fb1801d60101915caa6b6
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-08 00:23:48 +0600

    1.2.1 // Installation scripts // New user-friendly remove script

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Uninstalling rice-clock..."
+sudo rm -rf /usr/bin/rice-clock
+echo "Done!"
+echo "Hope you found my script interesting :("
+echo "If you wish to report any bugs, you're welcome to do it here - https://github.com/meth1337/rice-clock/issues"
+echo "Bye </3"
+
