2cb691e2410eea88cc890fb00ade73e347273cdf
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-08 00:32:40 +0600

    1.2.1 // Installation scripts // Fixes

--- src/install-depencies.sh
+++ src/install-depencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo pip install -r requirements.txt
+sudo apt install python3
+sudo apt install python3-pip
+sudo python3 -m pip install -r requirements.txt
 clear
 echo "Done!"
 echo "Now run build-binaries.sh. (Skip if you want to use pre-built binary, run create-command.sh in this case)"
