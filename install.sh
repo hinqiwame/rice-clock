6c3c63d04673d9d8eb3dcea56e1086f01e0f2fba
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-08 00:22:57 +0600

    1.2.1 // Installation scripts // New user-friendly install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Welcome to rice-clock installation!"
+echo "Giving permissions for script..."
+chmod +x rice-clock
+echo "Moving script to the system binaries directory..."
+sudo cp rice-clock /usr/bin/
+echo "Done! You now have rice-clock installed in your linux system."
+echo "If you're having any troubles in installation or using the script, report bugs to https://github.com/meth1337/rice-clock/issues"
+echo "<3"
+
