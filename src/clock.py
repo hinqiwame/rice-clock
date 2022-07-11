0cf79bf28e668fd2b00f72780072ad8e4b2e025e
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-11 08:56:07 +0600

    1.2.2 // Main script // Added 12h AM/PM format

--- src/clock.py
+++ src/clock.py
@@ -22,6 +22,7 @@ reset = Fore.RESET  # To return the base color
 # You can see preview of all fonts in README
 
 font = "banner"  # Default, you can change the name to switch the font
+ampm = False  # Change to True if you want the clock to be 12h AM/PM
 
 def clock(color):  # Defining a function
     print(color)  # Changing current terminal font color to chosen color
@@ -31,24 +32,51 @@ def clock(color):  # Defining a function
         tprint(datetime.now().strftime("%H:%M:%S"), font=font)  # Printing the time
         sleep(1)  # Making a delay in 1 second
 
+def clock_12h(color):
+    print(color)
+    system("clear")
+    while True:  # Creating a loop, that repeats itself forever (until it's stopped by user)
+        system("clear")  # Clearing the terminal to update the time
+        tprint(datetime.today().strftime("%I:%M:%S %p"), font=font)  # Printing the time
+        sleep(1)  # Making a delay in 1 second
+
+
 try:  # Opens try block to run the script options and work with issues below
     # Working with arguments
-    if sys.argv[1] == "-r":  # Runs script with red color
-        clock(color=red)
-    elif sys.argv[1] == "-g":  # Runs script with green color
-        clock(color=green)
-    elif sys.argv[1] == "-b":  # Runs script with blue color
-        clock(color=blue)
-    elif sys.argv[1] == "-p":  # Runs script with pink color
-        clock(color=pink)
-    elif sys.argv[1] == "-y":  # Runs script with yellow color
-        clock(color=yellow)
-    elif sys.argv[1] == "-c":  # Runs script with cyan color
-        clock(color=cyan)
-    elif sys.argv[1] == "-w":  # Runs script with white color
-        clock(color=white)
-    elif sys.argv[1] == "-h" or "--help":  # Help argument (Returns info on how to use the script)
-        print("A rice-like clock for terminals.\nFor usage, enter the command rice-clock + color, that you want to use\nAvailable colors:\n - r - red\n - g - green\n - b - blue\n - p - pink\n - y - yellow\n - c - cyan\n - w - white\n\nrice-clock homepage: https://github.com/meth1337/rice-clock\nReport bugs to https://github.com/meth1337/rice-clock/issues")
+    if not ampm:  # Checks if script is set to 12h ampm format
+        if sys.argv[1] == "-r":  # Runs script with red color
+            clock(color=red)
+        elif sys.argv[1] == "-g":  # Runs script with green color
+            clock(color=green)
+        elif sys.argv[1] == "-b":  # Runs script with blue color
+            clock(color=blue)
+        elif sys.argv[1] == "-p":  # Runs script with pink color
+            clock(color=pink)
+        elif sys.argv[1] == "-y":  # Runs script with yellow color
+            clock(color=yellow)
+        elif sys.argv[1] == "-c":  # Runs script with cyan color
+            clock(color=cyan)
+        elif sys.argv[1] == "-w":  # Runs script with white color
+            clock(color=white)
+        elif sys.argv[1] == "-h" or "--help":  # Help argument (Returns info on how to use the script)
+            print("A rice-like clock for terminals.\nFor usage, enter the command rice-clock + color, that you want to use\nAvailable colors:\n - r - red\n - g - green\n - b - blue\n - p - pink\n - y - yellow\n - c - cyan\n - w - white\n\nrice-clock homepage: https://github.com/meth1337/rice-clock\nReport bugs to https://github.com/meth1337/rice-clock/issues")
+    else:
+        if sys.argv[1] == "-r":  # Runs script with red color
+            clock_12h(color=red)
+        elif sys.argv[1] == "-g":  # Runs script with green color
+            clock_12h(color=green)
+        elif sys.argv[1] == "-b":  # Runs script with blue color
+            clock_12h(color=blue)
+        elif sys.argv[1] == "-p":  # Runs script with pink color
+            clock_12h(color=pink)
+        elif sys.argv[1] == "-y":  # Runs script with yellow color
+            clock_12h(color=yellow)
+        elif sys.argv[1] == "-c":  # Runs script with cyan color
+            clock_12h(color=cyan)
+        elif sys.argv[1] == "-w":  # Runs script with white color
+            clock_12h(color=white)
+        elif sys.argv[1] == "-h" or "--help":  # Help argument (Returns info on how to use the script)
+            print("A rice-like clock for terminals.\nFor usage, enter the command rice-clock + color, that you want to use\nAvailable colors:\n - r - red\n - g - green\n - b - blue\n - p - pink\n - y - yellow\n - c - cyan\n - w - white\n\nrice-clock homepage: https://github.com/meth1337/rice-clock\nReport bugs to https://github.com/meth1337/rice-clock/issues")
 except IndexError:  # If no arguments were entered (returns help argument value)
     print("A rice-like clock for terminals.\nFor usage, enter the command rice-clock + color, that you want to use\nAvailable colors:\n - r - red\n - g - green\n - b - blue\n - p - pink\n - y - yellow\n - c - cyan\n - w - white\n\nrice-clock homepage: https://github.com/meth1337/rice-clock\nReport bugs to https://github.com/meth1337/rice-clock/issues")
 except KeyboardInterrupt:  # If script was stopped by user
@@ -58,3 +86,4 @@ except KeyboardInterrupt:  # If script was stopped by user
 except:  # Except block for issues
     print(f"{red}Error!{reset}")
     sys.exit(0)
+    
