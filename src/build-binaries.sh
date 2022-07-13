94c59c78a14d0574dfa0b13778bfcf6d8c18395d
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-13 17:35:06 +0600

    1.2.3 // Installation scripts // Improved "documentation"

--- src/build-binaries.sh
+++ src/build-binaries.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "Compiling the file..."
 pyinstaller -F --clean clock.py
+clear
+echo "Removing dist/var files..."
 cd dist
 cp clock ..
 cd ..
@@ -7,5 +10,4 @@ rm -rf clock.spec dist build
 mv clock rice-clock
 clear
 echo "Done!"
-echo "Now run create-command.sh."
-
+echo "Now run create-command.sh to install new binary in your system."
