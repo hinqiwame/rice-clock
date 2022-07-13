bab13155169139e04723713667c14427ad38a846
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-13 17:33:58 +0600

    1.2.3 // Installation scripts // Improved "documentation"

--- src/create-command.sh
+++ src/create-command.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo "Copying binary into system binaries directory..."
 sudo cp rice-clock /usr/bin/
 cd /usr/bin/
+clear
+echo "Giving permissions for execution..."
 sudo chmod +x rice-clock
 clear
 echo "Done!"
-echo "Congrats! You now have rice-clock installed in your system!"
+echo "Congrats! You now have pyfetch installed in your system!"
+echo "(Location: /usr/bin/)"
