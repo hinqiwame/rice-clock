3ca9eb00fcb594b0fd965541443de16977da80c2
Amethist <77743895+meth1337@users.noreply.github.com>
2022-07-13 17:32:53 +0600

    1.2.3 // Installation scripts // Improved "documentation"

--- src/install-depencies.sh
+++ src/install-depencies.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+echo "Installing python3... (needs root password, the process will be skipped if python3 is already installed)"
 sudo apt install python3
+clear
+echo "Installing pip..."
 sudo apt install python3-pip
+clear
+echo "Installing all required modules..."
 sudo python3 -m pip install -r requirements.txt
 clear
 echo "Done!"
-echo "Now run build-binaries.sh. (Skip if you want to use pre-built binary, run create-command.sh in this case)"
+echo "Now run build-binaries.sh to build your binary.."
